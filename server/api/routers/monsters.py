d152c759618884b218366db9484bede70119d9d4
Niels-vdb <nielsvdbosch95@gmail.com>
2024-09-13 06:15:24 +0200

    UPDATE: post endpoint for monster API

--- server/api/routers/monsters.py
+++ server/api/routers/monsters.py
@@ -1,15 +1,46 @@
+from typing import Annotated, Any
+from pydantic import BaseModel, Field
+
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
+from sqlalchemy.exc import IntegrityError
 
 from server.api import get_db
 
-from ...database.models.monsters import Monster
+from server.database.models.characteristics import Size, Type
+from server.database.models.classes import Class, Subclass
+from server.database.models.effects import Effect
+from server.database.models.monsters import Monster
+from server.database.models.races import Race, Subrace
+from server.database.models.users import Party
 
 router = APIRouter(
     prefix="/api/monsters",
     tags=["Monsters"],
     responses={404: {"description": "Not found."}},
 )
+
+
+class MonsterBase(BaseModel):
+    name: Annotated[str, Field(min_length=1)]
+    description: str = None
+    information: str = None
+    alive: bool = None
+    active: bool = None
+    amour_class: int = None
+    image: bytes = None
+
+    race: int = None
+    subrace: int = None
+    size_id: int = None
+    type_id: int = None
+
+    parties: list[int] = None
+    classes: list[int] = None
+    subclasses: list[int] = None
+    immunities: list[int] = None
+    resistances: list[int] = None
+    vulnerabilities: list[int] = None
 
 
 @router.get("/")
@@ -45,3 +76,121 @@ def get_monster(monster_id: int, db: Session = Depends(get_db)):
         "immunities": monster.immunities,
         "vulnerabilities": monster.vulnerabilities,
     }
+
+
+@router.post("/")
+def post_monster(monster: MonsterBase, db: Session = Depends(get_db)):
+    attributes: dict[str, Any] = {}
+
+    if monster.description:
+        attributes["description"] = monster.description
+    if monster.information:
+        attributes["information"] = monster.information
+    if monster.alive:
+        attributes["alive"] = monster.alive
+    if monster.active:
+        attributes["active"] = monster.active
+    if monster.amour_class:
+        attributes["amour_class"] = monster.amour_class
+    if monster.image:
+        attributes["image"] = monster.image
+    if monster.race:
+        attributes["race"] = db.query(Race).filter(Race.id == monster.race).first()
+        if not attributes["race"]:
+            raise HTTPException(
+                status_code=404, detail="Race with this id does not exist."
+            )
+    if monster.subrace:
+        attributes["subrace"] = (
+            db.query(Subrace).filter(Subrace.id == monster.subrace).first()
+        )
+        if not attributes["subrace"]:
+            raise HTTPException(
+                status_code=404, detail="Subrace with this id does not exist."
+            )
+    if monster.size_id:
+        size = db.query(Size).filter(Size.id == monster.size_id).first()
+        if not size:
+            raise HTTPException(
+                status_code=404, detail="Size with this id does not exist."
+            )
+        attributes["size_id"] = size.id
+    if monster.type_id:
+        creature_type = db.query(Type).filter(Type.id == monster.type_id).first()
+        if not creature_type:
+            raise HTTPException(
+                status_code=404, detail="Type with this id does not exist."
+            )
+        attributes["type_id"] = creature_type.id
+    if monster.parties:
+        attributes["parties"] = [
+            db.query(Party).filter(Party.id == party).first()
+            for party in monster.parties
+        ]
+        for value in attributes["parties"]:
+            if value == None:
+                raise HTTPException(
+                    status_code=404, detail="Party with this id does not exist."
+                )
+    if monster.classes:
+        attributes["classes"] = [
+            db.query(Class).filter(Class.id == cls).first() for cls in monster.classes
+        ]
+        for value in attributes["classes"]:
+            if value == None:
+                raise HTTPException(
+                    status_code=404, detail="Class with this id does not exist."
+                )
+    if monster.subclasses:
+        attributes["subclasses"] = [
+            db.query(Subclass).filter(Subclass.id == subclass).first()
+            for subclass in monster.classes
+        ]
+        for value in attributes["subclasses"]:
+            if value == None:
+                raise HTTPException(
+                    status_code=404, detail="Subclass with this id does not exist."
+                )
+    if monster.immunities:
+        attributes["immunities"] = [
+            db.query(Effect).filter(Effect.id == immunity).first()
+            for immunity in monster.immunities
+        ]
+        for value in attributes["immunities"]:
+            if value == None:
+                raise HTTPException(
+                    status_code=404, detail="Effect with this id does not exist."
+                )
+    if monster.resistances:
+        attributes["resistances"] = [
+            db.query(Effect).filter(Effect.id == resistance).first()
+            for resistance in monster.resistances
+        ]
+        for value in attributes["resistances"]:
+            if value == None:
+                raise HTTPException(
+                    status_code=404, detail="Effect with this id does not exist."
+                )
+    if monster.vulnerabilities:
+        attributes["vulnerabilities"] = [
+            db.query(Effect).filter(Effect.id == vulnerability).first()
+            for vulnerability in monster.vulnerabilities
+        ]
+        for value in attributes["vulnerabilities"]:
+            if value == None:
+                raise HTTPException(
+                    status_code=404, detail="Effect with this id does not exist."
+                )
+    try:
+        new_monster = Monster(name=monster.name, **attributes)
+        db.add(new_monster)
+        db.commit()
+        db.refresh(new_monster)
+        return {
+            "message": f"New party '{new_monster.name}' has been added tot he database.",
+            "party": new_monster,
+        }
+    except Exception as e:
+        raise HTTPException(
+            status_code=400, detail=f"An unexpected error occurred. Error: {str(e)}"
+        )
--- server/api/tests/test_monsters.py
+++ server/api/tests/test_monsters.py
@@ -65,3 +65,188 @@ def test_get_no_monster(create_monster, db_session):
     response = client.get("/api/monsters/2")
     assert response.status_code == 404
     assert response.json() == {"detail": "Monster not found."}
+
+
+def test_post_monster(
+    create_size, create_type, create_party, create_effect, db_session
+):
+    response = client.post(
+        "/api/monsters",
+        json={
+            "name": "Baphomet",
+            "description": "A large minotaur looking devil",
+            "information": "Baphomet is no ordinary demon lord.",
+            "alive": True,
+            "active": True,
+            "armour_class": 22,
+            "size_id": 1,
+            "type_id": 1,
+            "parties": [1],
+            "resistances": [1],
+            "immunities": [1],
+            "vulnerabilities": [1],
+        },
+    )
+    assert response.status_code == 200
+    assert response.json() == {
+        "message": "New party 'Baphomet' has been added tot he database.",
+        "party": {
+            "id": 1,
+            "name": "Baphomet",
+            "size_id": 1,
+            "description": "A large minotaur looking devil",
+            "alive": True,
+            "creature": "monsters",
+            "active": True,
+            "armour_class": None,
+            "image": None,
+            "race": None,
+            "information": "Baphomet is no ordinary demon lord.",
+            "subrace": None,
+            "type_id": 1,
+        },
+    }
+
+
+def test_post_monster_fake_size(
+    create_size, create_type, create_party, create_effect, db_session
+):
+    response = client.post(
+        "/api/monsters",
+        json={
+            "name": "Baphomet",
+            "description": "A large minotaur looking devil",
+            "information": "Baphomet is no ordinary demon lord.",
+            "alive": True,
+            "active": True,
+            "armour_class": 22,
+            "size_id": 2,
+            "type_id": 1,
+            "parties": [1],
+            "resistances": [1],
+            "immunities": [1],
+            "vulnerabilities": [1],
+        },
+    )
+    assert response.status_code == 404
+    assert response.json() == {"detail": "Size with this id does not exist."}
+
+
+def test_post_monster_fake_type(
+    create_size, create_type, create_party, create_effect, db_session
+):
+    response = client.post(
+        "/api/monsters",
+        json={
+            "name": "Baphomet",
+            "description": "A large minotaur looking devil",
+            "information": "Baphomet is no ordinary demon lord.",
+            "alive": True,
+            "active": True,
+            "armour_class": 22,
+            "size_id": 1,
+            "type_id": 2,
+            "parties": [1],
+            "resistances": [1],
+            "immunities": [1],
+            "vulnerabilities": [1],
+        },
+    )
+    assert response.status_code == 404
+    assert response.json() == {"detail": "Type with this id does not exist."}
+
+
+def test_post_monster_fake_party(
+    create_size, create_type, create_party, create_effect, db_session
+):
+    response = client.post(
+        "/api/monsters",
+        json={
+            "name": "Baphomet",
+            "description": "A large minotaur looking devil",
+            "information": "Baphomet is no ordinary demon lord.",
+            "alive": True,
+            "active": True,
+            "armour_class": 22,
+            "size_id": 1,
+            "type_id": 1,
+            "parties": [2],
+            "resistances": [1],
+            "immunities": [1],
+            "vulnerabilities": [1],
+        },
+    )
+    assert response.status_code == 404
+    assert response.json() == {"detail": "Party with this id does not exist."}
+
+
+def test_post_monster_fake_resistance(
+    create_size, create_type, create_party, create_effect, db_session
+):
+    response = client.post(
+        "/api/monsters",
+        json={
+            "name": "Baphomet",
+            "description": "A large minotaur looking devil",
+            "information": "Baphomet is no ordinary demon lord.",
+            "alive": True,
+            "active": True,
+            "armour_class": 22,
+            "size_id": 1,
+            "type_id": 1,
+            "parties": [1],
+            "resistances": [2],
+            "immunities": [1],
+            "vulnerabilities": [1],
+        },
+    )
+    assert response.status_code == 404
+    assert response.json() == {"detail": "Effect with this id does not exist."}
+
+
+def test_post_monster_fake_immunity(
+    create_size, create_type, create_party, create_effect, db_session
+):
+    response = client.post(
+        "/api/monsters",
+        json={
+            "name": "Baphomet",
+            "description": "A large minotaur looking devil",
+            "information": "Baphomet is no ordinary demon lord.",
+            "alive": True,
+            "active": True,
+            "armour_class": 22,
+            "size_id": 1,
+            "type_id": 1,
+            "parties": [1],
+            "resistances": [1],
+            "immunities": [2],
+            "vulnerabilities": [1],
+        },
+    )
+    assert response.status_code == 404
+    assert response.json() == {"detail": "Effect with this id does not exist."}
+
+
+def test_post_monster_fake_vulnerabilities(
+    create_size, create_type, create_party, create_effect, db_session
+):
+    response = client.post(
+        "/api/monsters",
+        json={
+            "name": "Baphomet",
+            "description": "A large minotaur looking devil",
+            "information": "Baphomet is no ordinary demon lord.",
+            "alive": True,
+            "active": True,
+            "armour_class": 22,
+            "size_id": 1,
+            "type_id": 1,
+            "parties": [1],
+            "resistances": [1],
+            "immunities": [1],
+            "vulnerabilities": [2],
+        },
+    )
+    assert response.status_code == 404
+    assert response.json() == {"detail": "Effect with this id does not exist."}
