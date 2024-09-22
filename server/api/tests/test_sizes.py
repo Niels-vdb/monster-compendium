199f98929e8611e5d564f8b5101eaa7657a0150b
Niels-vdb <nielsvdbosch95@gmail.com>
2024-09-22 21:15:21 +0200

    UPDATE: Docstrings, logging and typing

--- server/api/tests/test_sizes.py
+++ server/api/tests/test_sizes.py
@@ -11,23 +11,21 @@ client = TestClient(app)
 def test_get_sizes(create_size, db_session):
     response = client.get("/api/sizes")
     assert response.status_code == 200
-    assert response.json() == {
-        "sizes": [
-            {"name": "Tiny", "id": 1},
-        ]
-    }
+    assert response.json() == [
+        {"name": "Tiny", "id": 1},
+    ]
 
 
 def test_get_no_sizes(db_session):
     response = client.get("/api/sizes")
-    assert response.status_code == 404
-    assert response.json() == {"detail": "No sizes found."}
+    assert response.status_code == 200
+    assert response.json() == []
 
 
 def test_get_size(create_size, db_session):
     response = client.get("/api/sizes/1")
     assert response.status_code == 200
-    assert response.json() == {"id": 1, "name": "Tiny", "creatures": [], "races": []}
+    assert response.json() == {"id": 1, "name": "Tiny"}
 
 
 def test_get_no_size(create_size, db_session):
@@ -43,7 +41,7 @@ def test_post_size(db_session):
             "size_name": "Medium",
         },
     )
-    assert response.status_code == 200
+    assert response.status_code == 201
     assert response.json() == {
         "message": "New size 'Medium' has been added to the database.",
         "size": {"id": 1, "name": "Medium"},
