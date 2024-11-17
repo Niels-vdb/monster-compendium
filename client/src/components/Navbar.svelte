f563082599c29b972d18f2731b3cfc92f45665fa
Niels-vdb <nielsvdbosch95@gmail.com>
2024-11-17 12:00:42 +0100

    UPDATE: Able to toggle creatures

--- client/src/components/Navbar.svelte
+++ client/src/components/Navbar.svelte
@@ -1,12 +1,28 @@
-<script>
+<script lang="ts">
 	import userIcon from '../assets/user.svg';
+
+	const creatureSwitch = (event: any) => {
+		for (let child of event.target.parentElement.children) {
+			child.classList.remove('border-black');
+			child.classList.remove('text-black');
+			child.classList.add('text-[#B3B3B3]');
+		}
+		event.target.classList.add('border-black');
+		event.target.classList.add('text-black');
+	};
 </script>
 
 <nav class=" grid h-24 grid-cols-6 items-center justify-items-stretch">
 	<div class="col-span-4 col-start-2 flex justify-center">
-		<button class="border-b border-black px-5 text-2xl text-black">Enemies</button>
-		<button class="border-b px-5 text-2xl text-[#B3B3B3]">Non Player Characters</button>
-		<button class="border-b px-5 text-2xl text-[#B3B3B3]">Player Characters</button>
+		<button onclick={creatureSwitch} class="border-b border-black px-5 text-2xl text-black"
+			>Enemies</button
+		>
+		<button onclick={creatureSwitch} class="border-b px-5 text-2xl text-[#B3B3B3]"
+			>Non Player Characters</button
+		>
+		<button onclick={creatureSwitch} class="border-b px-5 text-2xl text-[#B3B3B3]"
+			>Player Characters</button
+		>
 	</div>
 	<div
 		class="mr-5 grid w-60 grid-cols-8 items-center gap-5 justify-self-end rounded-xl bg-[#9B9B7A] p-2"
@@ -19,6 +35,3 @@
 		<button class="col-span-3 col-start-6 rounded-lg bg-[#2C2C2C] p-2 text-white">Logout</button>
 	</div>
 </nav>
-
-<style>
-</style>
